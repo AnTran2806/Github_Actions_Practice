de486c38253e3483c0fd4b35ca20a33450c00fd1
AnTran2806 <duyan20191999@gmail.com>
2026-03-07 19:52:24 +0700

    Practice_TCs

---
+++ tests/githubPractice.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test("Practice Test 1", async ({ page }) => {
+  console.log("Starting Practice Test 1");
+  await page.goto("https://www.saucedemo.com/");
+  console.log(await page.title());
+  expect(page).toHaveTitle("Swag Labs");
+  console.log("Ending Practice Test 1");
+});
+
+test("Practice Test 2", async ({ page }) => {
+  console.log("Starting Practice Test 2");
+  await page.goto("https://www.saucedemo.com/");
+  console.log(await page.title());
+  expect(page).toHaveTitle("Swag Labs");
+  console.log("Ending Practice Test 2");
+});
+
+test("Practice Test 3", async ({ page }) => {
+  console.log("Starting Practice Test 3");
+  await page.goto("https://www.saucedemo.com/");
+  console.log(await page.title());
+  expect(page).toHaveTitle("Swag Labs");
+  console.log("Ending Practice Test 3");
+});
